dbb42421b20d69236ab615d476c563b42aff3214
tomoki-yamamura <progtab17@gmail.com>
2022-11-12 01:06:40 +0900

    ad handler directry

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+	"github.com/tomoki-yamamura/practice-api/handlers"
 )
 
-func main()  {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-	
-	http.HandleFunc("/", helloHandler)
+func main() {
+
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
