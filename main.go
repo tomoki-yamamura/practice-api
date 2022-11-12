583103074f21603223cde454a2ad0269567a5926
tomoki-yamamura <progtab17@gmail.com>
2022-11-12 17:24:36 +0900

    add struct

--- main.go
+++ main.go
@@ -1,13 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
 	"github.com/gorilla/mux"
 	"github.com/tomoki-yamamura/practice-api/handlers"
 )
 
+type Comment struct {
+	CommentID int
+	ArticleID int
+	Message string
+	CratedAt time.Time
+}
+
+type Article struct {
+	ID int
+	Title string
+	Contents string
+	UserName string
+	NiceNum int
+	CommentList []Comment
+	CratedAt time.Time
+}
+
 func main() {
+	comment1 := Comment{
+		CommentID: 1,
+		ArticleID: 1,
+		Message: "test comment1",
+		CratedAt: time.Now(),
+	}
+	comment2 := Comment{
+		CommentID: 2,
+		ArticleID: 1,
+		Message: "second comment1",
+		CratedAt: time.Now(),
+	}
+	article := Article{
+		ID: 1,
+		Title: "first article",
+		Contents: "This is the test article.",
+		UserName: "saki",
+		NiceNum: 1,
+		CommentList: []Comment{comment1, comment2},
+		CratedAt: time.Now(),
+	}
+
+	fmt.Printf("%+v\n", article)
+	
 	r := mux.NewRouter()
 
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
