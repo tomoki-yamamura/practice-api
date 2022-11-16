dc01184e164c4828af994887da8c1a4cdc514af1
tomoki-yamamura <progtab17@gmail.com>
2022-11-16 22:47:23 +0900

    row

--- main.go
+++ main.go
@@ -20,28 +20,30 @@ func main() {
 	}
 	defer db.Close()
 
+
+	articleID := 5
 	const sqlStr = `
-	select title, contents, username, nice
-    from articles;
+		select *
+		from articles
+		where article_id = ?;
 	`
-	rows, err := db.Query(sqlStr)
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	var article models.Article
+	var createdTime sql.NullTime
+
+	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer rows.Close()
 
-	articleArray := make([]models.Article, 0)
-	for rows.Next() {
-		var article models.Article
-		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			articleArray = append(articleArray, article)
-		}
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
 	}
 
-	fmt.Printf("%+v\n", articleArray)
+	fmt.Printf("%+v\n", article)
 }
