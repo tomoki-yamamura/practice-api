063116511a228f1d841470ab7324955a6db20791
tomoki-yamamura <progtab17@gmail.com>
2022-11-16 22:20:56 +0900

    null ver

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"github.com/gorilla/mux"
-	"github.com/tomoki-yamamura/practice-api/handlers"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/tomoki-yamamura/practice-api/models"
 )
 
 func main() {
-	r := mux.NewRouter()
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	r.HandleFunc("/comment", handlers.PostCommentHandler)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
 
-	log.Println("server start port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	const sqlStr = `
+	select title, contents, username, nice
+    from articles;
+	`
+	rows, err := db.Query(sqlStr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			articleArray = append(articleArray, article)
+		}
+	}
+
+	fmt.Printf("%+v\n", articleArray)
 }
