b3f0c1df3648b64778d2ba276306667f16c11929
tomoki-yamamura <progtab17@gmail.com>
2022-11-23 15:26:41 +0900

    feta: test

--- repositories/articles.go
+++ repositories/articles.go
@@ -62,7 +62,7 @@ func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
 	}
 	var article models.Article
 	var createdTime sql.NullTime
-	err := row.Scan(&article.ID, &article.Title, &article.UserName, &article.NiceNum, &createdTime)
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
 	if err != nil {
 		return models.Article{}, err
 	}
